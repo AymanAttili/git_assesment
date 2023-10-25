b929b60da78ee457cc63a22081cbf9b6e035b58f
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2023-10-25 19:22:32 +0300

    MO forget to delete the parameter c

--- feature.ts
+++ feature.ts
@@ -1,4 +1,4 @@
 // Calculate the sum of two numbers
-function calculateSum(a: number, b: number, c: number): number {
+function calculateSum(a: number, b: number): number {
     return a + b + c;
 }
